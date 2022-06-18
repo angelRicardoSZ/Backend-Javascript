e41d48f2da6c608c3542ce16868529ea982fcd7a
angelRicardoSZ <angel.sanchezzefe@alumno.buap.mx>
2022-06-18 18:58:39 -0500

    routing and CRUD

--- store-project/index.js
+++ store-project/index.js
@@ -1,1 +1,20 @@
-console.log("Working")
+const express = require("express");
+const routerApi = require("./routes")
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+
+routerApi(app);
+// app.get("/", (req,res)=>{
+//   res.send("Working")
+// })
+
+
+
+
+
+app.listen(port, ()=>{
+  console.log("Puerto " + port)
+})
---
+++ store-project/routes/products.js
@@ -0,0 +1,82 @@
+const express = require("express");
+
+const { faker } = require("@faker-js/faker");
+
+const router = express.Router();
+
+router.get("/", (req,res)=>{
+  const { limit} = req.query;
+  const products = [];
+  for (let index = 0; index < limit; index++) {
+    products.push({
+      name: faker.commerce.productName(),
+      price : parseInt( faker.commerce.price(), 10),
+      image:faker.image.imageUrl(),
+    })
+
+  }
+  res.json(products)
+})
+
+
+
+// static
+router.get("/filter", (req,res) => {
+  res.send("Yo soy un filter")
+})
+// dynamic
+router.get("/:id", (req,res) => {
+  const { id } = req.params;
+  if(id==="999"){
+    res.status(404).json({
+      message:"not found"
+    })
+  } else {
+    res.status(200).json({
+      id,
+      name:"product",
+      price:2000
+
+    })
+  }
+
+})
+
+router.post("/", (req,res)=>{
+  const body = req.body;
+  console.log(body)
+  res.status(201).json(
+    {
+      message: "created",
+      data:body
+    }
+  )
+})
+
+
+router.patch("/:id", (req,res)=>{
+  const {id } = req.params;
+  const body = req.body;
+  console.log(body)
+  res.json(
+    {
+      message: "update",
+      data:body,
+      id,
+    }
+  )
+})
+
+router.delete("/:id", (req,res)=>{
+  const {id } = req.params;
+
+  res.json(
+    {
+      message: "deleted",
+
+      id,
+    }
+  )
+})
+
+module.exports = router;
