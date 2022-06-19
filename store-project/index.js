b5fb1a51e70bd73b56dbf56d3a39c7146beaafdf
angelRicardoSZ <angel.sanchezzefe@alumno.buap.mx>
2022-06-18 20:59:09 -0500

    solving cors problems and creating a whitelist

--- store-project/index.js
+++ store-project/index.js
@@ -1,10 +1,25 @@
 const express = require("express");
+const cors = require("cors")
 const routerApi = require("./routes")
 const { logErrors,errorHandler } = require("./middlewares/errorHandle")
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+
+// const whitelist = ["http://127.0.0.1:5500/index.html"]
+// const options = {
+//   origin: (origin, callback) => {
+//     if(whitelist.includes(origin)){
+//       callback(null, true)
+//     } else {
+//       callback(new Error("no permitido"))
+//     }
+//   }
+// }
+
+// app.use(cors(options))
+app.use(cors())
 
 
 routerApi(app);
