c8fc219713fe10d11ad55e898e35159cd14dc282
angelRicardoSZ <angel.sanchezzefe@alumno.buap.mx>
2022-07-04 08:58:44 -0500

    list, create(encrypted password), update (with token) and detail user

--- NodeSQL/store/mysql.js
+++ NodeSQL/store/mysql.js
@@ -34,7 +34,7 @@ function handleCon() {
 handleCon();
 
 
-function list(table,id) {
+function list(table) {
     return new Promise( (resolve, reject) => {
         connection.query(`SELECT * FROM ${table}`, (err, data) => {
             if (err) return reject(err);
@@ -42,6 +42,75 @@ function list(table,id) {
         })
     })
 }
+
+function get(table, id) {
+    return new Promise((resolve, reject) => {
+        connection.query(`SELECT * FROM ${table} WHERE id=${id}`, (err, data) => {
+            if (err) return reject(err);
+            resolve(data);
+        })
+    })
+}
+
+
+function insert(table, data) {
+    return new Promise((resolve, reject) => {
+        connection.query(`INSERT INTO ${table} SET ?`, data, (err, result) => {
+            if (err) return reject(err);
+            resolve(result);
+        })
+    })
+}
+
+function update(table, data) {
+    return new Promise((resolve, reject) => {
+        connection.query(`UPDATE ${table} SET ? WHERE id=?`, [data, data.id], (err, result) => {
+            if (err) return reject(err);
+            resolve(result);
+        })
+    })
+}
+
+async function upsert(table, data) {
+    const row = await get(table, data.id);
+    if (row.length === 0) {
+        return insert(table, data);
+    } else {
+        return update(table, data);
+    }
+    
+}
+
+function query(table, query) {
+    // let joinQuery = '';
+    // if (join) {
+    //     const key = Object.keys(join)[0];
+    //     const val = join[key];
+    //     joinQuery = `JOIN ${key} ON ${table}.${val} = ${key}.id`;
+    // }
+
+    return new Promise((resolve, reject) => {
+        connection.query(`SELECT * FROM ${table}  WHERE ?`, query, (err, res) => {
+            if (err) return reject(err);
+            let output = {
+                id: res[0].id,
+                username: res[0].username,
+                password: res[0].password
+            }
+            
+            resolve(output, null)
+            // resolve(res[0] || null);
+        })
+    })
+}
+
+
+
+
+
 module.exports = {
-    list
+    list,
+    get,
+    upsert,
+    query
 }
