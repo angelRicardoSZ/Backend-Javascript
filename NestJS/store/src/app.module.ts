7258155edee1f071d5f503c234769b957c34dabc
angelRicardoSZ <angel.sanchezzefe@alumno.buap.mx>
2022-07-10 07:48:33 -0500

    Adding users, customers and brands

--- NestJS/store/src/app.module.ts
+++ NestJS/store/src/app.module.ts
@@ -5,10 +5,16 @@ import { ProductsController } from './controllers/products.controller';
 import { CategoriesController } from './controllers/categories.controller';
 import { ProductsService } from './services/products.service';
 import { CategoriesService } from './services/categories.service';
+import { UsersController } from './controllers/users.controller';
+import { CustomersController } from './controllers/customers.controller';
+import { BrandsController } from './controllers/brands.controller';
+import { UsersService } from './services/users.service';
+import { CustomersService } from './services/customers.service';
+import { BrandsService } from './services/brands.service';
 
 @Module({
   imports: [],
-  controllers: [AppController, ProductsController, CategoriesController],
-  providers: [AppService, ProductsService, CategoriesService],
+  controllers: [AppController, ProductsController, CategoriesController, UsersController, CustomersController, BrandsController],
+  providers: [AppService, ProductsService, CategoriesService, UsersService, CustomersService, BrandsService],
 })
 export class AppModule {}
