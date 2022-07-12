743c0f25ffc9e26cb1ab3f4825bc6900ab2ea288
angelRicardoSZ <angel.sanchezzefe@alumno.buap.mx>
2022-07-12 07:04:42 -0500

    Consuming product module service in the user module

--- NestJS/store/src/users/users.module.ts
+++ NestJS/store/src/users/users.module.ts
@@ -3,8 +3,9 @@ import { UsersController } from './controllers/users.controller';
 import { CustomersController } from './controllers/customers.controller';
 import { UsersService } from './services/users.service';
 import { CustomersService } from './services/customers.service';
-
+import { ProductsModule } from '../products/products.module';
 @Module({
+  imports: [ProductsModule],
   controllers: [UsersController, CustomersController],
   providers: [UsersService, CustomersService],
 })
