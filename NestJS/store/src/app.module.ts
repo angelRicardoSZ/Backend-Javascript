4a9b02602ee4ea5612ce0c89ba772c04a8bf6a75
angelRicardoSZ <angel.sanchezzefe@alumno.buap.mx>
2022-07-12 07:48:56 -0500

    use Facotry

--- NestJS/store/src/app.module.ts
+++ NestJS/store/src/app.module.ts
@@ -1,17 +1,27 @@
-import { Module } from '@nestjs/common';
+import { Module, HttpModule, HttpService } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 const API_KEY = '123546';
 @Module({
-  imports: [UsersModule, ProductsModule],
+  imports: [HttpModule,UsersModule, ProductsModule],
   controllers: [AppController],
   providers: [
     AppService,
     {
       provide: 'API_KEY',
       useValue: API_KEY,
+    },
+    {
+      provide: 'TASKS',
+      useFactory:async (http: HttpService) => {
+        const tasks = await http
+          .get('https://jsonplaceholder.typicode.com/todos')
+          .toPromise();
+        return tasks.data;
+      },
+      inject: [HttpService],
     },
   ],
 })
